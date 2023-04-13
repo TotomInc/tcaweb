f5b5482c36ecb0e80251c0c95172ad723da04c7d
Thomas Cazade <cazade.thomas@gmail.com>
2023-04-13 16:33:55 +0200

    feat(Features): add features component on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,63 @@
+import {
+  DevicePhoneMobileIcon,
+  MagnifyingGlassIcon,
+  PaintBrushIcon,
+  PaperAirplaneIcon,
+  ChartBarIcon,
+  WrenchScrewdriverIcon,
+} from "@heroicons/react/24/outline";
+
 import { Hero } from "../components/sections/Hero";
+import { Features } from "../components/sections/Features";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+
+      <Features
+        className="mt-24 pb-24"
+        title="Votre solution digitale sur-mesure, tout-en-un"
+        description="Un site web permet d'activer votre présence numérique. C'est la première étape de votre visibilité en ligne."
+        features={[
+          {
+            title: "Site Responsive",
+            description:
+              "Le site s'adapte automatiquement à toutes les tailles d'écrans, afin d'accéder au contenu n'importe où.",
+            icon: DevicePhoneMobileIcon,
+          },
+          {
+            title: "Service SEO",
+            description:
+              "Le SEO (Search Engine Optimization) est un élément clé dans la réussite de votre présence numérique.",
+            icon: MagnifyingGlassIcon,
+          },
+          {
+            title: "Design sur-mesure",
+            description:
+              "Un design développé sur-mesure afin de correspondre aux besoins et à l'activité de votre entreprise.",
+            icon: PaintBrushIcon,
+          },
+          {
+            title: "Intégration de formulaires",
+            description:
+              "Laissez vos (futurs) clients vous contacter facilement grâce à l'intégration de formulaires sur votre site web.",
+            icon: PaperAirplaneIcon,
+          },
+          {
+            title: "Statistiques de performance",
+            description:
+              "Intégration d'une solution de statistiques web afin de monitorer les performances de votre site web.",
+            icon: ChartBarIcon,
+          },
+          {
+            title: "Maintenance & sécurité",
+            description:
+              "Forfait de maintenance de votre site web inclus pour l'hébergement, la sécurité et l'édition de contenu.",
+            icon: WrenchScrewdriverIcon,
+          },
+        ]}
+      />
+    </>
+  );
 }
---
+++ components/sections/Features.tsx
@@ -0,0 +1,55 @@
+import Balancer from "react-wrap-balancer";
+import cx from "classnames";
+
+export type FeaturesProps = {
+  className?: string;
+  title: string;
+  description: string;
+  features: {
+    title: string;
+    description: string;
+    icon: React.ElementType;
+  }[];
+};
+
+export function Features({
+  className,
+  title,
+  description,
+  features,
+}: FeaturesProps) {
+  return (
+    <section className={cx("relative w-full", className)}>
+      <div className="mx-auto max-w-7xl px-8">
+        <h2 className="mx-auto max-w-lg text-center text-3xl font-bold text-gray-950">
+          <Balancer>{title}</Balancer>
+        </h2>
+
+        <p className="mx-auto mt-8 max-w-lg text-center text-base leading-snug text-gray-800">
+          <Balancer>{description}</Balancer>
+        </p>
+
+        <dl className="mt-16 grid grid-cols-1 gap-y-12 md:grid-cols-2 md:gap-x-12 lg:grid-cols-3">
+          {features.map((feature) => (
+            <div
+              key={feature.title}
+              className="relative w-full rounded-md bg-gradient-to-br from-gray-50 to-gray-100 px-6 py-10"
+            >
+              <div className="absolute left-0 right-0 top-0 mx-auto -mt-6 flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-br from-blue-800 to-blue-600 shadow-sm">
+                <feature.icon className="block h-auto w-8 text-white" />
+              </div>
+
+              <dt className="text-center font-spline text-xl font-medium text-gray-950">
+                {feature.title}
+              </dt>
+
+              <dd className="mt-4 text-center text-base text-gray-500">
+                <Balancer>{feature.description}</Balancer>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </section>
+  );
+}
