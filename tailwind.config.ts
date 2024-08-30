994a2ba40c628a0b16be25407e555e21fef54986
Thomas Cazade <cazade.thomas@gmail.com>
2024-08-30 15:31:02 +0200

    chore: bump dependencies, remove unused files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        "sans": ["var(--font-inter)", ...fontFamily.sans],
-        "spline": ["var(--font-spline-sans)", ...fontFamily.sans],
-        "heading": ["var(--font-heading)", ...fontFamily.sans],
-
-        "landscaper-1": ["var(--font-inter)", ...fontFamily.sans],
-        "landscaper-1-heading": ["var(--font-dm-sans)", ...fontFamily.sans],
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+        spline: ["var(--font-spline-sans)", ...fontFamily.sans],
+        heading: ["var(--font-heading)", ...fontFamily.sans],
       },
     },
   },
