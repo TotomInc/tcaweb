66ab0cad7ca3b0bbfe96aabafa782663aec5fb5a
Thomas Cazade <cazade.thomas@gmail.com>
2025-01-28 11:32:39 +0100

    feat: add transition on BentoCard image loaded + inView

--- components/ui/BentoCard.tsx
+++ components/ui/BentoCard.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { AnimatePresence, motion, useInView } from "framer-motion";
 import Image from "next/image";
+import { useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface Props {
@@ -26,8 +30,13 @@ export function BentoCard({
   image,
   gradientSide,
 }: Props) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref);
+
   return (
     <div
+      ref={ref}
       className={twMerge(
         "relative flex w-full flex-col flex-nowrap gap-8 overflow-hidden rounded-2xl border border-gray-200 bg-white p-8 pb-0 shadow-xl shadow-gray-900/5 transition-all duration-300 ease-out hover:border-gray-300/75 hover:shadow-gray-900/10",
         className,
@@ -52,17 +61,27 @@ export function BentoCard({
       </div>
 
       {image ? (
-        <Image
-          src={image.src}
-          alt={image.alt}
-          width={image.width}
-          height={image.height}
-          quality={90}
-          className={twMerge(
-            "relative mx-auto mt-auto block h-auto w-full max-w-sm",
-            image.className,
-          )}
-        />
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, y: 24 }}
+            animate={imageLoaded && isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 24 }}
+            transition={{ duration: 0.5, ease: "easeOut", delay: 0.5 }}
+            className="relative"
+          >
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={image.width}
+              height={image.height}
+              quality={90}
+              className={twMerge(
+                "relative mx-auto mt-auto block h-auto w-full max-w-sm",
+                image.className,
+              )}
+              onLoad={() => setImageLoaded(true)}
+            />
+          </motion.div>
+        </AnimatePresence>
       ) : null}
 
       {children}
