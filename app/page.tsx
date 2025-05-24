79da02284f476cd7c6944dd1a069c904de911200
Thomas Cazade <cazade.thomas@gmail.com>
2025-05-24 09:52:53 +0200

    feat: add json-ld

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,25 @@ import { Portfolio } from "@/components/sections/Portfolio";
 import { WhyUs } from "@/components/sections/WhyUs";
 
 export default function Home() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    name: "TCA Web",
+    description: "Agence web spécialisée dans la création de sites pour PME",
+    url: "https://tcaweb.fr",
+    telephone: "+33642871966",
+    openingHours: "Mo-Fr 09:00-18:00",
+    logo: "https://tcaweb.fr/_static/images/logo-blue.png",
+  };
+
   return (
     <main className="bg-gray-100">
+      <script
+        // eslint-disable-next-line react-dom/no-dangerously-set-innerhtml
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        type="application/ld+json"
+      />
+
       <Hero />
       <Expertise />
       <How />
