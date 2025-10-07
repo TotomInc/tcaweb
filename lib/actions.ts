33ad49f92067d5d5c9f1db9a438954401c09da91
Thomas Cazade <cazade.thomas@gmail.com>
2025-10-07 15:29:33 +0200

    feat(datafast): add server-side tracking

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,8 +1,13 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 import { contactFormSchema } from "./schemas";
 
 export async function submitContactForm(formData: FormData) {
+  const cookieStore = await cookies();
+  const datafastVisitorId = cookieStore.get("datafast_visitor_id");
+
   const { organization, firstName, lastName, email, phone, message, website } =
     Object.fromEntries(formData);
 
@@ -47,6 +52,25 @@ export async function submitContactForm(formData: FormData) {
       return {
         error: "Une erreur est survenue lors de l'envoi du formulaire, veuillez réessayer.",
       };
+    }
+
+    // When form successfully submitted, we can send the data to Datafast to send a new goal.
+    if (datafastVisitorId?.value && process.env.DATAFAST_API_KEY) {
+      await fetch("https://datafa.st/api/v1/goals", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.DATAFAST_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          datafast_visitor_id: datafastVisitorId.value,
+          name: "contact_us",
+          metadata: {
+            name: `${validatedFields.data.firstName} ${validatedFields.data.lastName}`,
+            email: validatedFields.data.email,
+          },
+        }),
+      });
     }
   } catch {
     return {
